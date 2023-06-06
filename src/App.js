d84c684ebd2911c605a94c2d83e14bc029d8337d
Pakshaev <pakshaev2003@gmail.com>
2023-06-06 14:20:18 +0400

    empty comment

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Register from "./pages/register/Register";
-
+//
 function App() {
   // return <Home />;
   return (
